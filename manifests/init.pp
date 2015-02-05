959ec4b719dc0cf59329772f90f2bd6920e3e7de
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-05 16:20:02 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,10 +37,9 @@
 #
 class role_test (
   $parameters = parseyaml(file('/etc/puppet/hieradata/server-test.yaml')) 
-  $configfile = $parameters['role_test::configfile']
   ) {
   
- 
+  $configfile = $parameters['role_test::configfile']
   notice( "$configfile" )
 }
 
