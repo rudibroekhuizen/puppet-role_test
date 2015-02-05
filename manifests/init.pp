6ec3177595c51d3e49a3576f431fc49fcc416c75
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-05 16:48:05 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,8 +38,10 @@
 class role_test (
   $parameters = parseyaml(file('/etc/puppet/hieradata/server-test.yaml')) #parseyaml based with the Foreman
   #$configfile = 'logstash-snmpget-01.conf' #hiera based, without the Foreman
-  $configfile = $parameters['role_test::configfile'] #parseyaml based with the Foreman
+ 
   ) {
+ 
+  $configfile = $parameters['role_test::configfile'] #parseyaml based with the Foreman 
   
   notice( "$configfile" )
   notice( "$hiera" )
