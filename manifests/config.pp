55821b55fb01c28beabcdad1d3b76fedf4ddb9b3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-06 16:43:46 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,17 @@ class role_test::config (
   $configfile,
   ) {
   
+  case $deployment {
+    'foreman': {
+      notice( "foreman" )
+      $configfile = $parameters['role_test::configfile']
+    }
+    'masterless': {
+      notice( "masterless" )
+      $configfile = hiera('role_test::configfile')
+    }
+  }
+  
   notice( "$configfile" )
 
 }
