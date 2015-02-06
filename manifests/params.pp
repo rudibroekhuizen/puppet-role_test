cdb276aaa4e99ac9a0bac3f75166041cf1718e6b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-06 12:31:10 +0100

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,16 @@
 # == Class: role_test::params
 #
 class role_test::params {
-  $deployment  = 'foreman'
+  $deployment  = 'masterless'
   $parameters  = parseyaml(file('/etc/puppet/hieradata/server-test.yaml'))
   
   case $deployment {
     'foreman': { 
-      $configfile = $parameters['role_test::configfile']
+      $configfile = $parameters['role_test::configfile'],
     }
     'masterless': {
-      $configfile = 'logstash-snmpget-01.conf'
+      $configfile = 'logstash-snmpget-01.conf',
+      notice( "params.pp" )
     }
   }
   
