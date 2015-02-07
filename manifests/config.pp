1172619353050e63420fcdd8798051b92d327931
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-07 13:36:13 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 # == Class: role_test::config
 #
 class role_test::config (
-  $configfile = $parameters['role_test::configfile'],
+  $configfile = $::parameters['role_test::configfile'],
   ) {
   
   notice( "$configfile" )
