95c053d884aafd9e7b58fcfde044983d1e110679
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-10 14:09:16 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,18 @@
 # == Class: role_test
 #
-class role_test {
-  
-  $parameters = parseyaml('
+class role_test (
+  $data_source = '
 ---
 role_test::configfile:
   example.yaml
 ---
-')
-  
+') {
+
+$parameters = parseyaml(puppet:///modules/role_test/data_source)
+notice( "$data_source" )
+notice( "$parameters" ) 
+notice( "$configfile" )
+
   class { 'role_test::config':
   }
   
