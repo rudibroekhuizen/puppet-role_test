f5a7042c891bdc02ce8584cf56c16a9cab383adb
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-11 13:50:24 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,10 +4,10 @@ class role_test::config (
   $configfile = $role_test::yaml::parameters['role_test::configfile']
   ) {
   
-  notice( "$configfile" )
-  
   file { "/tmp/configfile":
     content => "$configfile",
   }
+
+  notice( "$configfile" )
 
 }
